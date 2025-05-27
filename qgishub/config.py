be6d6e4b2ac945983d59425d8ca9c86cda7490a3
yama-kei <yamazaki@mierune.co.jp>
2025-05-27 14:38:19 +0900

    metadata: プラグイン名と説明を「STRATO」に更新し、リポジトリURLを追加

--- qgishub/config.py
+++ qgishub/config.py
@@ -8,12 +8,8 @@ class Config:
     API_URL: str = "https://d3eqzgssnrhp33.cloudfront.net/api"
 
 
-config = Config()
-
-"""
 config = Config(
     COGNITO_URL="https://qgishubv3.auth.ap-northeast-1.amazoncognito.com",
     COGNITO_CLIENT_ID="49fnn61i1bh3jongq62i290461",
     API_URL="http://localhost:3000/api",
 )
-"""
