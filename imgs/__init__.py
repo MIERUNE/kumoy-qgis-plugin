46966fce91e3187331d65679f48818a709de6fd5
Kanahiro <mediterranean1769@gmail.com>
2025-10-14 14:47:24 +0900

    feat: iconをFigma準拠に

--- imgs/__init__.py
+++ imgs/__init__.py
@@ -1,3 +1,9 @@
 import os
 
+from qgis.PyQt.QtGui import QIcon
+
 IMGS_PATH = os.path.dirname(os.path.realpath(__file__))
+
+MAP_ICON = QIcon(os.path.join(IMGS_PATH, "map.svg"))
+RELOAD_ICON = QIcon(os.path.join(IMGS_PATH, "reload.svg"))
+VECTOR_ICON = QIcon(os.path.join(IMGS_PATH, "vector.svg"))
