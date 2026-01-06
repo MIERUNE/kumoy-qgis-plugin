1dc306880c9898b0a32b8ef6af066b8d357bc7dc
Kanahiro <mediterranean1769@gmail.com>
2026-01-06 16:01:15 +0900

    fix: 使うsignalを変更してみる

--- ui/layer_indicators.py
+++ ui/layer_indicators.py
@@ -1,10 +1,10 @@
-from qgis.PyQt.QtCore import QTimer
 from qgis.core import QgsProject
 from qgis.gui import QgsLayerTreeViewIndicator
+from qgis.PyQt.QtCore import QTimer
 from qgis.utils import iface
 
-from .icons import MAIN_ICON
 from ..kumoy.constants import DATA_PROVIDER_KEY
+from .icons import MAIN_ICON
 
 
 def update_kumoy_indicator():
@@ -12,14 +12,11 @@ def update_kumoy_indicator():
     root = QgsProject.instance().layerTreeRoot()
     view = iface.layerTreeView()
 
-    missing_nodes = False
     for layer in QgsProject.instance().mapLayers().values():
         if layer.providerType() != DATA_PROVIDER_KEY:
             continue
         node = root.findLayer(layer.id())
         if not node:
-            # node made missing when project is loading, retry later
-            missing_nodes = True
             continue
         if _has_kumoy_indicator(node):
             continue
@@ -27,10 +24,6 @@ def update_kumoy_indicator():
         indicator.setToolTip("Kumoy layer")
         indicator.setIcon(MAIN_ICON)
         view.addIndicator(node, indicator)
-
-    # HACK: retry in 100ms if nodes were not yet created when loading map
-    if missing_nodes:
-        QTimer.singleShot(100, update_kumoy_indicator)
 
 
 def _has_kumoy_indicator(node):
